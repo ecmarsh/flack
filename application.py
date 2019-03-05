b8a5c7f85aea9a72c684206b92f243f32a0d36c3
Ethans-Mac-mini <ethancmarsh@yahoo.com>
2019-03-04 23:26:49 -0800

    Skeleton

--- application.py
+++ application.py
@@ -1,13 +1,29 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_assets import Bundle, Environment
 from flask_socketio import SocketIO, emit
 
+# __Init__
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+
+# Scss --> css
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle(
+    'sass/main.scss',
+    filters='pyscss',
+    depends=('**/*.scss'),
+    output='styles.css')
+assets.register('scss_all', scss)
 
 
 @app.route("/")
 def index():
     return "Project 2: TODO"
+
+
+if __name__ == '__main___':
+    socketio.run(app)
---
+++ static/js/socket.js
@@ -0,0 +1,4 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port)
+socket.on('connect', function() {
+	socket.emit('my event', { data: "I'm connected!" })
+})
